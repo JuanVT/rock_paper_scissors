c620c68c86398bf98d79b5c23bd0d5f17835ecbd
Tirso Rodriguez <tirso.rod@gmail.com>
2017-09-12 09:26:29 +0100

    First game implementation

--- Rock_paper_scissors_lizard_Spock.py
+++ Rock_paper_scissors_lizard_Spock.py
@@ -2,48 +2,25 @@
 
 import random
 
-
-def name_to_number(name):
-
-    if name == "rock":
-        return 0
-    elif name == "spock":
-        return 1
-    elif name == "paper":
-        return 2
-    elif name == "lizard":
-        return 3
-    elif name == "scissors":
-        return 4
-    else:
-        return "This Name is not valid!"
+game_elements = ['rock', 'paper', 'scissors', 'lizard', 'spock']
 
 
-def number_to_name(number):
+def game_logic():
+    print "Select your pick:"
+    player_choice = raw_input()
 
-    if number == 0:
-        return "rock"
-    elif number == 1:
-        return "spock"
-    elif number == 2:
-        return "paper"
-    elif number == 3:
-        return "lizard"
-    elif number == 4:
-        return "scissors"
-    else:
-        return "This Number is not valid!"
-
-
-def game_logic(player_choice):
+    if player_choice not in game_elements:
+        print "That choice is not available, zoquete! Try again..."
+        return game_logic()
 
     print "Player has chosen: " + player_choice
 
-    player_number = name_to_number(player_choice)
+    player_number = game_elements.index(player_choice)
 
     comp_number = random.randrange(0, 4)
 
-    comp_choice = number_to_name(comp_number)
+    comp_choice = game_elements[comp_number]
+
     print "Computer has chosen: " + comp_choice
     print
 
@@ -61,6 +38,4 @@ def game_logic(player_choice):
 
     print
 
-print "Select your pick:"
-
-game_logic(raw_input())
+game_logic()
